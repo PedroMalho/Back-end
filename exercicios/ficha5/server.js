1078d928bb899828f1d1c7fc2b5583b2a748cf57
PedroMalho <pmvmalho@gmail.com>
2019-04-09 21:32:02 +0100

    exercicios ficha5

--- exercicios/ficha5/server.js
+++ exercicios/ficha5/server.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function readFile(fileName){
+    var file = fs.readFileSync(fileName);
+    return file;
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+readFile("./persons.json");
+
+app.get('/users', function(req, res){
+    res.send(readFile("./persons.json"));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
